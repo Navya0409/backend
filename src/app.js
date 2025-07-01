87e7a21e188fdcb1204abb35fec5fc1b497e019b
Navya <navyagarg0409@gmail.com>
2025-07-01 11:43:44 +0530

    user controller

--- src/app.js
+++ src/app.js
@@ -8,9 +8,12 @@ app.use(cors ({
     credentials : true
 }))
 
-app.use(express.json({limit : "16kb"})) //limit on the amount of data coming up
+app.use(express.json({limit : "32kb"})) //limit on the amount of data coming up
 app.use(express.urlencoded({extended:true , limit :"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js"
+app.use("/api/v1/users" , userRouter)
 
 export {app};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
-import express from "express";
+// import express from "express"; // Remove this line
 import connectDB from "../src/db/index.js";
-const app = express();
+import { app } from "./app.js"; // Add this line
 dotenv.config({
     path: './.env'
 });
@@ -16,32 +16,3 @@ connectDB()
 .catch((error) => {
     console.log("MongoDB connection failed" , error);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-// const app = express();
-
-// (async () => {
-//     try {
-//         await connectDB();
-//         app.on("error", (error) => {
-//             console.log("Server error:", error);
-//             throw error;
-//         });
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port ${process.env.PORT}`);
-//         });
-//     } catch (error) {
-//         console.log("Error:", error);
-//         throw error;
-//     }
-// })();
